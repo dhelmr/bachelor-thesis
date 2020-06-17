47ddb5259c7ea8703d4fa0b70701a2dca77d06b7
Daniel Helmrich <dahelmrich@gmail.com>
2020-06-17 14:44:00 +0200

    Fix doc2vec vector inferring and store doc2vec after training in file

--- anomaly_detection/feature_extractors/doc2vec_packets.py
+++ anomaly_detection/feature_extractors/doc2vec_packets.py
@@ -1,4 +1,5 @@
 import logging
+import os
 import typing as t
 
 import dpkt as dpkt
@@ -31,22 +32,33 @@ class PacketDoc2Vec(FeatureExtractor):
     def __init__(self):
         self.statistic_features_extractor = BasicPacketFeatureExtractor()
         self.model = None
+        self.model_dir = ".packet_doc2vec_cache"
+        if not os.path.exists(self.model_dir):
+            os.mkdir(self.model_dir)
 
     def fit_extract(self, pcap_file: str) -> np.ndarray:
         return self.get_features(pcap_file, train_before=True)
 
     def get_features(self, pcap_file: str, train_before: bool):
+        model_file = self.get_model_file(pcap_file)
+        if self.model is None and os.path.exists(model_file):
+            logging.info("Found model file %s, load...", model_file)
+            self.model = Doc2Vec.load(model_file)
+
         if not train_before and self.model is None:
             raise RuntimeError("Error, Doc2Vec model is not yet trained. Abort.")
         packet_infos = self._read_packets(pcap_file)
         logging.info("Finished loading the packets into memory.")
         payloads, statistics = zip(*packet_infos)
         doc_gen = DocumentGenerator(payloads)
-        if train_before:
+        if train_before and self.model is None:
             logging.info("Start training doc2vec model")
-            self.model = Doc2Vec(doc_gen, vector_size=20, window=4, min_count=1, workers=128)
+            self.model = Doc2Vec(doc_gen, vector_size=20, window=5, min_count=4, workers=128)
             logging.info("Finished training doc2vec model")
-        doc2vec_features = list(map(lambda x: self.model.infer_vector(x), doc_gen))
+            self.model.save(open(model_file, "wb"))
+            logging.info("Stored doc2vec model to %s", model_file)
+        logging.info("Infer vectors...")
+        doc2vec_features = [self.model.infer_vector(x.words) for x in doc_gen]
         return np.array(doc2vec_features)  # TODO add statistic features as well
 
     def _read_packets(self, pcap_file: str) -> t.List[PacketInformation]:
@@ -76,6 +88,9 @@ class PacketDoc2Vec(FeatureExtractor):
 
     def get_name(self) -> str:
         return "packet_doc2vec"
+
+    def get_model_file(self, pcap: str) -> str:
+        return os.path.join(self.model_dir, "doc2vec_%s.model" % os.path.basename(pcap))
 
 
 def read_pcap_pcapng(file):
