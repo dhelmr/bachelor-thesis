b0d8887a38f5a0910cff48ca9c476e5452d30917
Daniel Helmrich <dahelmrich@gmail.com>
2020-06-19 19:41:21 +0200

    Simplify preprocessing and include packets if their id is duplicated as well

--- dataset_utils/preprocess_ids2017.py
+++ dataset_utils/preprocess_ids2017.py
@@ -5,6 +5,7 @@ from datetime import datetime
 import dpkt as dpkt
 
 from anomaly_detection.types import DatasetPreprocessor
+from dataset_utils import pcap_utils
 from dataset_utils.cic_ids_2017 import *
 
 FILES = {
@@ -72,75 +73,72 @@ class CICIDS2017Preprocessor(DatasetPreprocessor):
             logging.info("Process file %s", pcap_file)
             attacks_packet_ids = self.get_attacks_packet_ids(label_file)
             labels = self.generate_labels(pcap_file, attacks_packet_ids)
-            pandas_dict = {packet_id: [packet_id, label] for packet_id, label in labels.items()}
-            df = pandas.DataFrame.from_dict(pandas_dict, orient="index", columns=["packet_id", "label"])
+            df = pandas.DataFrame.from_records(labels, columns=["packet_id", "label"])
             output_csv_path = os.path.join(dataset_path, os.path.basename(pcap_file) + "_packet_labels.csv")
             logging.info("Write packet labels into %s", label_file)
             df.to_csv(output_csv_path, index=False)
 
     def generate_labels(self, pcap_file: str, attack_times: pandas.DataFrame) \
-            -> t.Dict[PacketID, TrafficType]:
-        packets = dpkt.pcapng.Reader(open(pcap_file, "rb"))
-        labelled_packets: t.Dict[PacketID, TrafficType] = dict()
+            -> t.List[t.Tuple[PacketID, TrafficType]]:
+        packets = pcap_utils.read_pcap_pcapng(pcap_file)
+        labelled_packets: t.List[t.Tuple[PacketID, TrafficType]] = list()
         progress = 0
         for timestamp, buf in packets:
             flow_ids = make_flow_ids(timestamp, buf)
             packet_id = get_packet_id(timestamp, buf, flow_ids)
             if (flow_ids is None) or \
                     not (flow_ids[0] in attack_times.index or flow_ids[1] in attack_times.index):
-                labelled_packets[packet_id] = TrafficType.BENIGN.value
+                traffic_type = TrafficType.BENIGN
             else:
                 # check for attack times
-                labelled_packets[packet_id] = TrafficType.ATTACK.value
+                traffic_type = TrafficType.ATTACK
+            entry = (packet_id, traffic_type.value)
+            labelled_packets.append(entry)
             if progress % 50000 == 0:
                 logging.info("Processed %s packets...", progress)
             progress += 1
         return labelled_packets
 
+    def get_attacks_packet_ids(self, flows_file: str) -> pandas.DataFrame:
+        df = read_csv(flows_file)  # TODO
+        attacks = df.loc[df["Label"] != BENIGN_LABEL]
+        benigns = df.loc[df["Label"] == BENIGN_LABEL]
+        attacks["reverse_flow_id"] = attacks.index.map(lambda flow: self.reverse_flow_id(flow))
+        in_both = pandas.merge(attacks, benigns, how="inner", left_index=True, right_index=True)
+        in_both_reversed_id = pandas.merge(attacks, benigns, how="inner", left_on="reverse_flow_id", right_index=True)
+        in_both = pandas.concat([in_both, in_both_reversed_id])
+        benign_times = in_both["Timestamp_y"].groupby(in_both.index).apply(
+            lambda elements: [self.date_to_timestamp(formatted_date) for formatted_date in list(elements)]
+        )
+        attack_times = attacks["Timestamp"].groupby(attacks.index).apply(
+            lambda elements: [self.date_to_timestamp(formatted_date) for formatted_date in list(elements)]
+        )
+        result_df = pandas.merge(attack_times, benign_times, how="left", right_index=True, left_index=True)
+        result_df.rename(columns={"Timestamp_y": "benign", "Timestamp": "attack"}, inplace=True)
+        return result_df
 
-def get_attacks_packet_ids(self, flows_file: str) -> pandas.DataFrame:
-    df = read_csv(flows_file)  # TODO
-    attacks = df.loc[df["Label"] != BENIGN_LABEL]
-    benigns = df.loc[df["Label"] == BENIGN_LABEL]
-    attacks["reverse_flow_id"] = attacks.index.map(lambda flow: self.reverse_flow_id(flow))
-    in_both = pandas.merge(attacks, benigns, how="inner", left_index=True, right_index=True)
-    in_both_reversed_id = pandas.merge(attacks, benigns, how="inner", left_on="reverse_flow_id", right_index=True)
-    in_both = pandas.concat([in_both, in_both_reversed_id])
-    benign_times = in_both["Timestamp_y"].groupby(in_both.index).apply(
-        lambda elements: [self.date_to_timestamp(formatted_date) for formatted_date in list(elements)]
-    )
-    attack_times = attacks["Timestamp"].groupby(attacks.index).apply(
-        lambda elements: [self.date_to_timestamp(formatted_date) for formatted_date in list(elements)]
-    )
-    result_df = pandas.merge(attack_times, benign_times, how="left", right_index=True, left_index=True)
-    result_df.rename(columns={"Timestamp_y": "benign", "Timestamp": "attack"}, inplace=True)
-    return result_df
+    def pcap_and_label_files(self, dataset_path: str) -> t.List[t.Tuple[str, str]]:
+        return [(os.path.join(dataset_path, pcap), os.path.join(dataset_path, label_file)) \
+                for pcap, label_file in FILES.items()]
 
+    @staticmethod
+    def reverse_flow_id(flow_id: str):
+        splitted = flow_id.split("-")
+        if len(splitted) != 5:
+            return "<invalid flow>"
+        src_ip, dest_ip, src_port, dest_port, protocol = splitted
+        return format_flow_id(src_ip, dest_ip, src_port, dest_port, protocol, reverse=True)
 
-def pcap_and_label_files(self, dataset_path: str) -> t.List[t.Tuple[str, str]]:
-    return [(os.path.join(dataset_path, pcap), os.path.join(dataset_path, label_file)) \
-            for pcap, label_file in FILES.items()]
-
-
-@staticmethod
-def reverse_flow_id(flow_id: str):
-    splitted = flow_id.split("-")
-    if len(splitted) != 5:
-        return "<invalid flow>"
-    src_ip, dest_ip, src_port, dest_port, protocol = splitted
-    return format_flow_id(src_ip, dest_ip, src_port, dest_port, protocol, reverse=True)
-
-
-def date_to_timestamp(self, datestr: str):
-    date_part, time_part = datestr.split(" ")
-    d, m, y = date_part.split("/")
-    if len(d) != 2:
-        d = "0" + d
-    if len(m) != 2:
-        m = "0" + m
-    date_part = f"{d}/{m}/{y}"
-    if time_part[1] == ":":
-        time_part = "0" + time_part
-    datestr = f"{date_part} {time_part}"
-    result = datetime.strptime(datestr, "%d/%m/%Y %I:%M")  # example "5/7/2017 8:42" = 5th of July 2017
-    return result
+    def date_to_timestamp(self, datestr: str):
+        date_part, time_part = datestr.split(" ")
+        d, m, y = date_part.split("/")
+        if len(d) != 2:
+            d = "0" + d
+        if len(m) != 2:
+            m = "0" + m
+        date_part = f"{d}/{m}/{y}"
+        if time_part[1] == ":":
+            time_part = "0" + time_part
+        datestr = f"{date_part} {time_part}"
+        result = datetime.strptime(datestr, "%d/%m/%Y %I:%M")  # example "5/7/2017 8:42" = 5th of July 2017
+        return result
