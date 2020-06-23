a6efb3ee3031fccf496f6b3ab00f0f466ced4102
Daniel Helmrich <dahelmrich@gmail.com>
2020-06-23 15:24:46 +0200

    Add fix for pool connections with big data

--- anomaly_detection/feature_extractors/doc2vec_packets.py
+++ anomaly_detection/feature_extractors/doc2vec_packets.py
@@ -1,6 +1,9 @@
+import functools
 import logging
 import multiprocessing
 import os
+import struct
+import sys
 import typing as t
 
 import dpkt as dpkt
@@ -10,6 +13,8 @@ from gensim.models.doc2vec import Doc2Vec, TaggedDocument
 from anomaly_detection.feature_extractors.basic_packet_feature_extractor import BasicPacketFeatureExtractor
 from anomaly_detection.types import FeatureExtractor, TrafficType
 from dataset_utils.pcap_utils import read_pcap_pcapng
+
+logger = logging.getLogger()
 
 
 class PacketInformation(t.NamedTuple):
@@ -45,6 +50,7 @@ class PacketDoc2Vec(FeatureExtractor):
         self.model_dir = ".packet_doc2vec_cache"
         if not os.path.exists(self.model_dir):
             os.mkdir(self.model_dir)
+        patch_mp_connection_bpo_17560()
 
     def fit_extract(self, pcap_file: str) -> np.ndarray:
         if self.model is not None:
@@ -163,3 +169,61 @@ class LogIter:
             if process % self.log_after == 0:
                 logging.info(self.format, process)
             yield item
+
+
+def patch_mp_connection_bpo_17560():
+    """Apply PR-10305 / bpo-17560 connection send/receive max size update
+
+    See the original issue at https://bugs.python.org/issue17560 and
+    https://github.com/python/cpython/pull/10305 for the pull request.
+
+    This only supports Python versions 3.3 - 3.7, this function
+    does nothing for Python versions outside of that range.
+
+    """
+    patchname = "Multiprocessing connection patch for bpo-17560"
+    if not (3, 3) < sys.version_info < (3, 8):
+        logger.info(
+            patchname + " not applied, not an applicable Python version: %s",
+            sys.version
+        )
+        return
+
+    from multiprocessing.connection import Connection
+
+    orig_send_bytes = Connection._send_bytes
+    orig_recv_bytes = Connection._recv_bytes
+    if (
+            orig_send_bytes.__code__.co_filename == __file__
+            and orig_recv_bytes.__code__.co_filename == __file__
+    ):
+        logger.info(patchname + " already applied, skipping")
+        return
+
+    @functools.wraps(orig_send_bytes)
+    def send_bytes(self, buf):
+        n = len(buf)
+        if n > 0x7fffffff:
+            pre_header = struct.pack("!i", -1)
+            header = struct.pack("!Q", n)
+            self._send(pre_header)
+            self._send(header)
+            self._send(buf)
+        else:
+            orig_send_bytes(self, buf)
+
+    @functools.wraps(orig_recv_bytes)
+    def recv_bytes(self, maxsize=None):
+        buf = self._recv(4)
+        size, = struct.unpack("!i", buf.getvalue())
+        if size == -1:
+            buf = self._recv(8)
+            size, = struct.unpack("!Q", buf.getvalue())
+        if maxsize is not None and size > maxsize:
+            return None
+        return self._recv(size)
+
+    Connection._send_bytes = send_bytes
+    Connection._recv_bytes = recv_bytes
+
+    logger.info(patchname + " applied")
