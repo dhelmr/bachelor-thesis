0c93212afc771b0a6aa7696f127f031af6fd878c
Daniel Helmrich <dahelmrich@gmail.com>
2020-10-19 21:20:18 +0200

    Add netflows.py (CLI interface for netflow extraction)

---
+++ netflows.py
@@ -0,0 +1,38 @@
+import argparse
+
+import pandas
+
+from anomaly_detection.feature_extractors.basic_netflow_extractor import BasicNetflowFeatureExtractor
+from anomaly_detection.types import TrafficSequence
+from dataset_utils import pcap_utils
+
+
+class PcapTrafficReader:
+
+    def read(self, pcap_file):
+        return TrafficSequence(
+            name=pcap_file,
+            is_consistent=False,
+            ids=[],
+            labels=pandas.Series(dtype=float),
+            packet_reader=pcap_utils.read_pcap_pcapng(pcap_file, print_progress_after=200)
+        )
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-p", "--pcap", help="Pcap file to read", required=True)
+    parser.add_argument("-o", "--output", help="CSV output file to write", required=True)
+    BasicNetflowFeatureExtractor.init_parser(parser)
+    parsed = parser.parse_args()
+    fe = BasicNetflowFeatureExtractor.init_by_parsed(parsed)
+
+    traffic = PcapTrafficReader().read(parsed.pcap)
+    features = fe.extract_features(traffic)
+    df = features.as_pandas()
+    df.to_csv(parsed.output)
+    print(df)
+
+
+if __name__ == '__main__':
+    main()
