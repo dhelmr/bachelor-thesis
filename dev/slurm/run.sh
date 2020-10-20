edfc5921d6d35ba11b8a4ca82f8299004b3fd5ce
Daniel Helmrich <dahelmrich@gmail.com>
2020-10-20 10:54:04 +0200

    wait 30 minutes

--- dev/slurm/run.sh
+++ dev/slurm/run.sh
@@ -8,10 +8,11 @@ cd "$BASEDIR/../.." || exit
 
 mkdir slurmlogs
 
-for f in ./hypertune/*; do
-  for i in {0..1000}; do
+for i in {0..1000}; do
+  for f in ./hypertune/*; do
     NAME="$f_$i_$ARGS"
     JOB_ARGS="$ARGS --only-id=$i"
     sbatch --job-name="$NAME.run" --output="slurmlogs/$NAME.out" --export="HYPERTUNE_ARGS=$JOB_ARGS" "$JOB_FILE"
   done
+  sleep 1800
 done
