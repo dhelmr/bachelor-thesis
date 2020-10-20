97ba9afb80323624359c974cfaa059d4ef2b2795
Daniel Helmrich <dahelmrich@gmail.com>
2020-10-20 12:45:46 +0200

    use max-jobs for slurm

--- dev/slurm/iterate_hypertune.sh
+++ dev/slurm/iterate_hypertune.sh
@@ -16,10 +16,11 @@ for i in {0..1000}; do
     echo "Start $NAME with '$JOB_ARGS'"
     sbatch --output="slurmlogs/%j.out" --export="HYPERTUNE_ARGS=$JOB_ARGS" "$JOB_FILE"
   done
+  sleep 10
   CURRENT_JOBS=$(sacct --allocation | grep -c -E "(RUNNING|PENDING)")
   while [ $CURRENT_JOBS -ge $MAX_JOB_COUNT ]; do
     sleep 5
     CURRENT_JOBS=$(sacct --allocation | grep -c -E "(RUNNING|PENDING)")
   done
-  sleep 3
+
 done
