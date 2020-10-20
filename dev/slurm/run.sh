08da5ae91ebe35ee63fe479aeef16970e0b5d279
Daniel Helmrich <dahelmrich@gmail.com>
2020-10-20 10:58:57 +0200

    log ourput

--- dev/slurm/run.sh
+++ dev/slurm/run.sh
@@ -12,6 +12,7 @@ for i in {0..1000}; do
   for f in ./hypertune/*; do
     NAME="$f_$i_$ARGS"
     JOB_ARGS="$ARGS --only-id=$i"
+    echo "Start $NAME with '$JOB_ARGS'"
     sbatch --job-name="$NAME.run" --output="slurmlogs/$NAME.out" --export="HYPERTUNE_ARGS=$JOB_ARGS" "$JOB_FILE"
   done
   sleep 1800
