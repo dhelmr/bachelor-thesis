4927ef74e390c75195048650de3bcb364e587314
Daniel Helmrich <dahelmrich@gmail.com>
2020-10-20 11:28:13 +0200

    Add run_all.sh for slurm

--- dev/slurm/iterate_hypertune.sh
+++ dev/slurm/iterate_hypertune.sh
@@ -11,9 +11,9 @@ mkdir -p slurmlogs
 for i in {0..1000}; do
   for f in ./hypertune/*; do
     NAME="$f-$i-$ARGS"
-    JOB_ARGS="$ARGS --only-id=$i -f $f"
+    JOB_ARGS="$ARGS --only-index=$i -f $f"
     echo "Start $NAME with '$JOB_ARGS'"
-    sbatch --job-name="$NAME.run" --output="slurmlogs/$NAME.out" --export="HYPERTUNE_ARGS='$JOB_ARGS'" "$JOB_FILE"
+    sbatch --output="slurmlogs/%a.out" --export="HYPERTUNE_ARGS=$JOB_ARGS" "$JOB_FILE"
   done
   sleep 1800
 done
---
+++ dev/slurm/run_all.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+BASEDIR=$(dirname "$0")
+EXEC="$BASEDIR/iterate_hypertune.sh"
+
+$EXEC --db work/sqlite.db --dataset cic-ids-2017 --subset wednesday
+$EXEC --db work/sqlite.db --dataset unsw-nb15 --src data/unsw-nb15 --subset 1-5/10,11,12,20,34,55
