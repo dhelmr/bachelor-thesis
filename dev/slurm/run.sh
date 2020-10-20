7ee41c537f22c6b7fe0479972f1b1ac1eb7a9443
Daniel Helmrich <dahelmrich@gmail.com>
2020-10-20 11:04:42 +0200

    fix bash syntax

--- dev/slurm/run.sh
+++ dev/slurm/run.sh
@@ -2,15 +2,15 @@
 
 BASEDIR=$(dirname "$0")
 JOB_FILE="$BASEDIR/hypertune.job"
-ARGS="$@"
+ARGS="*@"
 
 cd "$BASEDIR/../.." || exit
 
-mkdir slurmlogs
+mkdir -p slurmlogs
 
 for i in {0..1000}; do
   for f in ./hypertune/*; do
-    NAME="$f_$i_$ARGS"
+    NAME="$f-$i-$ARGS"
     JOB_ARGS="$ARGS --only-id=$i"
     echo "Start $NAME with '$JOB_ARGS'"
     sbatch --job-name="$NAME.run" --output="slurmlogs/$NAME.out" --export="HYPERTUNE_ARGS=$JOB_ARGS" "$JOB_FILE"
