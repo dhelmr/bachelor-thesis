967e612a7deecd866c0f2d5d3e8ac57125bc2298
Daniel Helmrich <dahelmrich@gmail.com>
2020-10-20 11:52:18 +0200

    fix

--- dev/slurm/iterate_hypertune.sh
+++ dev/slurm/iterate_hypertune.sh
@@ -11,7 +11,7 @@ mkdir -p slurmlogs
 for i in {0..1000}; do
   for f in ./hypertune/*; do
     NAME="$f-$i-$ARGS"
-    JOB_ARGS="$ARGS --only-index $i -f $f"
+    JOB_ARGS="--only-index $i -f $f $ARGS"
     echo "Start $NAME with '$JOB_ARGS'"
     sbatch --output="slurmlogs/%j.out" --export="HYPERTUNE_ARGS=$JOB_ARGS" "$JOB_FILE"
   done
