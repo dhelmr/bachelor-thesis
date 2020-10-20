1973725adc47032b6639338ab4edc3082a3f8c0e
Daniel Helmrich <dahelmrich@gmail.com>
2020-10-20 12:10:34 +0200

    fix unsw path for slurm

--- dev/slurm/run_all.sh
+++ dev/slurm/run_all.sh
@@ -4,4 +4,4 @@ BASEDIR=$(dirname "$0")
 EXEC="$BASEDIR/iterate_hypertune.sh"
 
 ($EXEC --db data/sqlite.db --dataset cic-ids-2017 --subset wednesday) &
-$EXEC --db data/sqlite.db --dataset unsw-nb15 --src data/unsw-nb15 --subset 1-5/10,11,12,20,34,55
+$EXEC --db data/sqlite.db --dataset unsw-nb15 --src unsw-nb15 --subset 1-5/11,12,20,34,55
