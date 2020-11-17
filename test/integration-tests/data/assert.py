3f95b9e826eaedefbbb542860d0fe0a317e559e3
Daniel Helmrich <dahelmrich@gmail.com>
2020-11-17 17:08:39 +0100

    Add hypertune, list-* commands to integration tests

--- test/integration-tests/data/assert.py
+++ test/integration-tests/data/assert.py
@@ -8,13 +8,16 @@ parser.add_argument("arg1")
 parser.add_argument("--lt")
 parser.add_argument("--gt")
 parser.add_argument("--eq")
+parser.add_argument("--not-in")
 parser.add_argument("--msg")
 
 args = parser.parse_args()
 
 
 def fail(cond):
-    print(f"{args.msg + ' -- ' if args.msg is not None else ''} Assertation failed: {cond}")
+    print(
+        f"{args.msg + ' -- ' if args.msg is not None else ''} Assertation failed: {cond}"
+    )
     sys.exit(1)
 
 
@@ -24,3 +27,5 @@ if args.gt is not None and not args.arg1 > args.gt:
     fail(f"{args.arg1} > {args.gt}")
 if args.eq is not None and not args.arg1 == args.eq:
     fail(f"{args.arg1} == {args.eq}")
+if args.not_in is not None and str(args.arg1).lower() in str(args.not_in).lower():
+    fail(f"{args.arg1} not in {args.not_in}")
--- test/integration-tests/data/integration_test.sh
+++ test/integration-tests/data/integration_test.sh
@@ -11,7 +11,7 @@ set -o pipefail
 cp /data/assert.py /app
 cd /app
 
-python bin/run_canids migrate-db --db /sqlite.db
+bin/run_canids migrate-db --db /sqlite.db
 python bin/run_canids list-fe
 python bin/run_canids list-de
 python bin/run_canids train --dataset unsw-nb15 --subset tiny --db /sqlite.db -m model-test-1234
@@ -21,4 +21,29 @@ python bin/run_canids classify --dataset unsw-nb15 --subset tiny --db /sqlite.db
 python bin/run_canids classify --dataset unsw-nb15 --subset tiny --db /sqlite.db --id model-test-1234/cl-2 -m model-test-1234
 python bin/run_canids evaluate --dataset unsw-nb15 --subset tiny --db /sqlite.db --id model-test-1234/cl-2
 python bin/run_canids evaluate --dataset unsw-nb15 --subset tiny --db /sqlite.db --id model-test-1234/cl-1
-python bin/run_canids migrate-db --db /sqlite.db
+
+
+# just test whether all hyperparameter configurations can be run without problems
+for f in hypertune/activated/*.json; do
+  python bin/run_canids hypertune --dataset unsw-nb15 --subset tiny --db /sqlite.db -f "$f"
+  python bin/run_canids hypertune --dataset unsw-nb15 --subset tiny --db /sqlite.db -f "$f" --only-index 1
+done
+
+VIS_DIR=$(mktemp -d)
+bin/run_canids list-de -s | xargs -L 1 bin/run_canids visualize -o "$VIS_DIR" --model-part-name
+bin/run_canids list-fe -s | xargs -L 1 bin/run_canids visualize -o "$VIS_DIR" --model-part-name
+
+bin/run_canids stats --dataset unsw-nb15 --subset tiny
+
+# list evaluation for every model
+OUTPUT="$(bin/run_canids list-models | cut -f 1 -d " " | tail -n +3 | xargs -L 1 bin/run_canids list-evaluations --db /sqlite.db --model )"
+./assert.py "None" --not-in "$OUTPUT"
+
+# list evaluation for every classification
+OUTPUT="$(bin/run_canids list-classifications | cut -f 1 -d " " | tail -n +3 | xargs -L 1 bin/run_canids list-evaluations --db /sqlite.db --id )"
+./assert.py "None" --not-in "$OUTPUT"
+
+# should check if it still works; should not do anything
+./assert.py "" --eq "$(bin/run_canids migrate-db --db /sqlite.db)"
+
+bin/run_canids --help
