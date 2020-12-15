068377a15707dcc1810e34312014a888d526c670
Daniel Helmrich <dahelmrich@gmail.com>
2020-12-15 17:31:41 +0100

    Filter out invalid values (NAN, INF) for metrics and group by training set

--- canids/report.py
+++ canids/report.py
@@ -28,20 +28,32 @@ class ReportGenerator:
         self, model_part_name: str, output_path: str, output_format: ReportOutputFormat
     ):
         records, hyperparams = self.db.get_evaluations_by_model_part(model_part_name)
-        part_names = records["part_name"].unique()
-        report = {
-            part_name: self._sort_group(records[records["part_name"] == part_name])
-            for part_name in part_names
-        }
+        report = {}
+        for training_set in records["traffic_name"].unique():
+            training_set_records = records[records["traffic_name"] == training_set]
+            part_names = training_set_records["part_name"].unique()
+            report.update(
+                {
+                    f"{training_set}_{part_name}": self._sort_group(
+                        training_set_records[
+                            training_set_records["part_name"] == part_name
+                        ]
+                    )
+                    for part_name in part_names
+                }
+            )
+        print(report)
         if output_format is ReportOutputFormat.JSON:
             with open(output_path, "w") as f:
                 json.dump(report, f)
         elif output_format is ReportOutputFormat.CSV:
             for part_name, metrics in report.items():
+                part_name_esc = part_name.replace("/", "_")
                 for metric_name, best_evaluations in metrics.items():
                     df = pandas.DataFrame(best_evaluations)
                     path = os.path.join(
-                        output_path, f"{model_part_name}_{part_name}_{metric_name}.csv"
+                        output_path,
+                        f"{model_part_name}_{part_name_esc}_{metric_name}.csv",
                     )
                     df.to_csv(path)
 
@@ -49,9 +61,9 @@ class ReportGenerator:
         sorted_by_metrics = [
             (
                 metric_name,
-                records.sort_values(by=[metric_name], ascending=False).head(
-                    self.best_n
-                ),
+                records[(records[metric_name] >= 0) | (records[metric_name] <= 1)]
+                .sort_values(by=[metric_name], ascending=False)
+                .head(self.best_n),
             )
             for metric_name in self.sort_by_metrics
         ]
