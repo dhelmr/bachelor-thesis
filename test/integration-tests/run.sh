d8cb672b3bcc526e409ae4d1008aaf3a38768bb6
Daniel Helmrich <dahelmrich@gmail.com>
2021-04-09 12:16:16 +0200

    Fix integration test

--- test/integration-tests/run.sh
+++ test/integration-tests/run.sh
@@ -5,7 +5,7 @@ set -o pipefail
 
 cd $(dirname "$0")
 
-docker run -v $(pwd)/data:/data -v -it --entrypoint /data/integration_test.sh canids:latest
+docker run -v $(pwd)/data:/data -v -it --entrypoint /data/integration_test.sh dhelmr/canids:latest
 
 data/assert.py 0 --eq "$?" --msg "Integration test failed!"
 echo "Integration tests ran successfully"
